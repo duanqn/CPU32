5e2e4c053709daf1d97469968cd45b9144ed8e07
潘星宇 <pxy14@mails.tsinghua.edu.cn>
2016-11-11 03:26:11 +0800

    finish openmips.vhd

--- src/mem_wb.vhd
+++ src/mem_wb.vhd
@@ -4,15 +4,23 @@ ENTITY mem_wb is
   port(
     rst: in STD_LOGIC;
     clk: in STD_LOGIC;
-    -- 访存阶段结果
+    -- input
     mem_wd: in STD_LOGIC_VECTOR(4 downto 0);
     mem_wreg: in STD_LOGIC;
     mem_wdata: in STD_LOGIC_VECTOR(31 downto 0);
+    mem_hi: in STD_LOGIC_VECTOR(31 downto 0);
+    mem_lo: in STD_LOGIC_VECTOR(31 downto 0);
+    mem_whilo: in STD_LOGIC;
     
-    -- 送到回写阶段的信�    
-	 wb_wd: out STD_LOGIC_VECTOR(4 downto 0);
+    -- output
+	  wb_wd: out STD_LOGIC_VECTOR(4 downto 0);
     wb_wreg: out STD_LOGIC;
-    wb_wdata: out STD_LOGIC_VECTOR(31 downto 0));
+    wb_wdata: out STD_LOGIC_VECTOR(31 downto 0);
+    wb_hi: out STD_LOGIC_VECTOR(31 downto 0);
+    wb_lo: out STD_LOGIC_VECTOR(31 downto 0);
+    wb_whilo: out STD_LOGIC
+    );
+
 end mem_wb;
 
 architecture arch of mem_wb is
@@ -25,10 +33,16 @@ begin
         wb_wd <= "00000";
         wb_wreg <= '0';
         wb_wdata <= x"00000000";
+        wb_hi <= x"00000000";
+        wb_lo <= x"00000000";
+        wb_whilo <= '0';
       else
         wb_wd <= mem_wd;
         wb_wreg <= mem_wreg;
         wb_wdata <= mem_wdata;
+        wb_hi <= mem_hi;
+        wb_lo <= mem_lo;
+        wb_whilo <= mem_whilo;
       end if;
     end if;  
   end process ; -- identifier
