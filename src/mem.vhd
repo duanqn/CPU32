a7000135d20c41a73f3f87f1b0132ccbc3a20642
Max Duan <duanqn@yeah.net>
2016-11-11 02:02:28 +0800

    Add move instructions for mem

--- src/mem.vhd
+++ src/mem.vhd
@@ -6,27 +6,39 @@ ENTITY mem is
 		wd_i: in STD_LOGIC_VECTOR(4 downto 0);
 		wreg_i: in STD_LOGIC;
 		wdata_i: in STD_LOGIC_VECTOR(31 downto 0);
+    hi_i: in STD_LOGIC_VECTOR(31 downto 0);
+    lo_i: in STD_LOGIC_VECTOR(31 downto 0);
+    whilo_i: in STD_LOGIC;
 		-- 访存阶段结果
     wd_o: out STD_LOGIC_VECTOR(4 downto 0);
     wreg_o: out STD_LOGIC;
-    wdata_o: out STD_LOGIC_VECTOR(31 downto 0));
+    wdata_o: out STD_LOGIC_VECTOR(31 downto 0);
+    hi_o: out STD_LOGIC_VECTOR(31 downto 0);
+    lo_o: out STD_LOGIC_VECTOR(31 downto 0);
+    whilo_o:out STD_LOGIC
+    );
 end mem;
 
 architecture arch of mem is
-  --signal 
+  --signal
 begin
   identifier : process( rst, wd_i, wreg_i, wdata_i )
   begin
-    if(rst='1') then 
+    if(rst='1') then
       wd_o <= "00000";
       wreg_o <= '0';
       wdata_o <= x"00000000";
+      hi_o <= x"00000000";
+      lo_o <= x"00000000";
+      whilo_o <= '0';
     else
       wd_o <= wd_i;
       wreg_o <= wreg_i;
       wdata_o <= wdata_i;
-    end if;  
+      hi_o <= hi_i;
+      lo_o <= lo_i;
+      whilo_o <= whilo_i;
+    end if;
   end process ; -- identifier
 
 end architecture ; -- arch
-
