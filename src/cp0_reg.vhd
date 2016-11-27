9591a0279efa4715effd28e633ed7d3e49524af5
Max Duan <duanqn@yeah.net>
2016-11-27 20:26:38 +0800

    Add exception for CP0

--- src/cp0_reg.vhd
+++ src/cp0_reg.vhd
@@ -28,7 +28,11 @@ entity cp0_reg is
     Cause_o : out std_logic_vector(31 downto 0);
     EPC_o : out std_logic_vector(31 downto 0);
     EBase_o : out std_logic_vector(31 downto 0);
-    timer_int_o : out std_logic
+    timer_int_o : out std_logic;
+
+    excepttype_i: in STD_LOGIC_VECTOR(31 downto 0);
+    current_inst_address_i: in STD_LOGIC_VECTOR(31 downto 0);
+    is_in_delayslot_i: in STD_LOGIC
   );
 end cp0_reg;
 
@@ -80,7 +84,7 @@ begin
 
     elsif rising_edge(clk) then
 
-      register_values(12) <= register_values(12) + 1;
+      register_values(9) <= register_values(9) + 1;
       register_values(13)(15 downto 10) <= int_i;
 
       if(register_values(11) /= X"00000000" and register_values(9) = register_values(11)) then
@@ -93,39 +97,143 @@ begin
         end if;
       end if;
 
-      if(we_i = '1') then
-        case waddr_i is
-          --Index
-          when "00000" => register_values(0)(31) <= data_i(31);
-                          register_values(0)(5 downto 0) <= data_i(5 downto 0);
-          --EntryLo0
-          when "00010" => register_values(2)(25 downto 0) <= data_i(25 downto 0);
-          --EntryLo1
-          when "00011" => register_values(3)(25 downto 0) <= data_i(25 downto 0);
-          --BadVAddr
-          when "01000" => register_values(8) <= data_i;
-          --Count
-          when "01001" => register_values(9) <= data_i;
-          --EntryHi
-          when "01010" => register_values(10)(31 downto 13) <= data_i(31 downto 13);
-                          register_values(10)(7 downto 0) <= data_i(7 downto 0);
-          --Compare
-          when "01011" => register_values(11) <= data_i;
-          --Status
-          when "01100" => register_values(12) <= data_i;
-          --EPC
-          when "01110" => register_values(14) <= data_i;
-          --Cause
-          when "01101" => register_values(13)(9 downto 8) <= data_i(9 downto 8);
-                          register_values(13)(23) <= data_i(23);
-                          register_values(13)(22) <= data_i(22);
-                          register_values(13)(27) <= data_i(27);
-          --EBase
-          when "01111" => register_values(15)(29 downto 12) <= data_i(29 downto 12);
-          when others => null;
+      if(excepttype_i = x"00000000") then
+        if(we_i = '1') then
+          case waddr_i is
+            --Index
+            when "00000" => register_values(0)(31) <= data_i(31);
+                            register_values(0)(5 downto 0) <= data_i(5 downto 0);
+            --EntryLo0
+            when "00010" => register_values(2)(25 downto 0) <= data_i(25 downto 0);
+            --EntryLo1
+            when "00011" => register_values(3)(25 downto 0) <= data_i(25 downto 0);
+            --BadVAddr
+            when "01000" => register_values(8) <= data_i;
+            --Count
+            when "01001" => register_values(9) <= data_i;
+            --EntryHi
+            when "01010" => register_values(10)(31 downto 13) <= data_i(31 downto 13);
+                            register_values(10)(7 downto 0) <= data_i(7 downto 0);
+            --Compare
+            when "01011" => register_values(11) <= data_i;
+            --Status
+            when "01100" => register_values(12) <= data_i;
+            --EPC
+            when "01110" => register_values(14) <= data_i;
+            --Cause
+            when "01101" => register_values(13)(9 downto 8) <= data_i(9 downto 8);
+                            register_values(13)(23) <= data_i(23);
+                            register_values(13)(22) <= data_i(22);
+                            register_values(13)(27) <= data_i(27);
+            --EBase
+            when "01111" => register_values(15)(29 downto 12) <= data_i(29 downto 12);
+            when others => null;
+          end case;
+        end if;
+      else
+        if(we_i = '1') then
+          case waddr_i is
+            --Index
+            when "00000" => register_values(0)(31) <= data_i(31);
+                            register_values(0)(5 downto 0) <= data_i(5 downto 0);
+            --EntryLo0
+            when "00010" => register_values(2)(25 downto 0) <= data_i(25 downto 0);
+            --EntryLo1
+            when "00011" => register_values(3)(25 downto 0) <= data_i(25 downto 0);
+            --BadVAddr
+            when "01000" => register_values(8) <= data_i;
+            --Count
+            when "01001" => register_values(9) <= data_i;
+            --EntryHi
+            when "01010" => register_values(10)(31 downto 13) <= data_i(31 downto 13);
+                            register_values(10)(7 downto 0) <= data_i(7 downto 0);
+            --Compare
+            when "01011" => register_values(11) <= data_i;
+            --EBase
+            when "01111" => register_values(15)(29 downto 12) <= data_i(29 downto 12);
+            when others => null;
+          end case;
+        end if;
+
+        case excepttype_i is
+          when x"00000001" => --Interrupt
+            register_values(12)(1) <= '1';
+            register_values(13)(6 downto 2) <= "00000";
+            if(is_in_delayslot_i = '1') then
+              register_values(14) <= current_inst_address_i - 4;
+              register_values(13)(31) <= '1';
+            else
+              register_values(14) <= current_inst_address_i;
+              register_values(13)(31) <= '0';
+            end if;
+          when x"00000008" => --Syscall
+
+            if(register_values(12)(1) = '0') then
+              if(is_in_delayslot_i = '1') then
+                register_values(14) <= current_inst_address_i - 4;
+                register_values(13)(31) <= '1';
+              else
+                register_values(14) <= current_inst_address_i;
+                register_values(13)(31) <= '0';
+              end if;
+              register_values(12)(1) <= '1';
+              register_values(13)(6 downto 2) <= "01000";
+            else
+              register_values(13)(6 downto 2) <= "01000";
+            end if;
+
+          when x"0000000A" => --Invalid instruction
+            if(register_values(12)(1) = '0') then
+              if(is_in_delayslot_i = '1') then
+                register_values(14) <= current_inst_address_i - 4;
+                register_values(13)(31) <= '1';
+              else
+                register_values(14) <= current_inst_address_i;
+                register_values(13)(31) <= '0';
+              end if;
+              register_values(12)(1) <= '1';
+              register_values(13)(6 downto 2) <= "01010";
+            else
+              register_values(13)(6 downto 2) <= "01010";
+            end if;
+          when x"0000000D" => --Trap
+            if(register_values(12)(1) = '0') then
+              if(is_in_delayslot_i = '1') then
+                register_values(14) <= current_inst_address_i - 4;
+                register_values(13)(31) <= '1';
+              else
+                register_values(14) <= current_inst_address_i;
+                register_values(13)(31) <= '0';
+              end if;
+              register_values(12)(1) <= '1';
+              register_values(13)(6 downto 2) <= "01101";
+            else
+              register_values(13)(6 downto 2) <= "01101";
+          end if;
+
+          when x"0000000C" => --Overflow
+            if(register_values(12)(1) = '0') then
+              if(is_in_delayslot_i = '1') then
+                register_values(14) <= current_inst_address_i - 4;
+                register_values(13)(31) <= '1';
+              else
+                register_values(14) <= current_inst_address_i;
+                register_values(13)(31) <= '0';
+              end if;
+              register_values(12)(1) <= '1';
+              register_values(13)(6 downto 2) <= "01100";
+            else
+              register_values(13)(6 downto 2) <= "01100";
+          end if;
+
+          when x"0000000E" => --Eret
+            register_values(12)(1) <= '0';
+
+          when others => NULL;
         end case;
       end if;
-    end if;
+
+    end if; -- rising_edge
   end process;
 
   read_operation : process(clk, rst)
