5724dfec64652a8f17e33f3ac197bafe2255534e
Max Duan <duanqn@yeah.net>
2016-12-08 02:20:25 +0800

    Modify ram_simulate Backup rom_Fibo

--- testbench/ram_simulate.vhd
+++ testbench/ram_simulate.vhd
@@ -1,28 +1,28 @@
 --------------------------------------------------------------------------------
--- Company: 
+-- Company:
 -- Engineer:
 --
 -- Create Date:   00:24:19 12/03/2016
--- Design Name:   
+-- Design Name:
 -- Module Name:   D:/CPU/DWP/test1.vhd
 -- Project Name:  DWP
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
+-- Target Device:
+-- Tool versions:
+-- Description:
+--
 -- VHDL Test Bench Created by ISE for module: DWPC
--- 
+--
 -- Dependencies:
--- 
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
 --
--- Notes: 
+-- Notes:
 -- This testbench has been automatically generated using types std_logic and
 -- std_logic_vector for the ports of the unit under test.  Xilinx recommends
 -- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
+-- to guarantee that the testbench will bind correctly to the post-implementation
 -- simulation model.
 --------------------------------------------------------------------------------
 LIBRARY ieee;
@@ -30,11 +30,11 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE ieee.std_logic_TEXTIO.all;
 USE STD.TextIO.All;
- 
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
- 
+
 ENTITY ram_simulate IS
   Port(
     baseram_addr : IN  std_logic_vector(19 downto 0);
@@ -51,8 +51,8 @@ ENTITY ram_simulate IS
     );
 
 END ram_simulate;
- 
-ARCHITECTURE behavior OF ram_simulate IS 
+
+ARCHITECTURE behavior OF ram_simulate IS
 
 type mem_array is array(1023 downto 0) of std_logic_vector(31 downto 0);
 signal memory: mem_array := (others => (others => '0'));
@@ -60,27 +60,30 @@ signal memory: mem_array := (others => (others => '0'));
 constant DELAY: time := 10 ns;
 
 BEGIN
-   baseram_data <= transport memory(to_integer(unsigned(baseram_addr(9 downto 0)))) after DELAY when baseram_ce = '0' and baseram_oe = '0' and baseram_we = '1' else (others => 'Z');
-   extraram_data <= transport memory(to_integer(unsigned('1' & extraram_addr))) after DELAY when extraram_ce = '0' and extraram_oe = '0' and extraram_we = '1' else (others => 'Z');
 
-process(baseram_we, baseram_data_in, baseram_addr, baseram_oe, baseram_ce)
+process(baseram_we, baseram_data, baseram_addr, baseram_oe, baseram_ce)
 begin
     -- Write to baseMemory
-        if (baseram_ce = '0' and baseram_oe = '1' and baseram_we = '0') then 
-            memory(to_integer(unsigned(baseram_addr(9 downto 0)))) <= baseram_data;
-            report "write base " & integer'image(to_integer(unsigned(baseram_addr(9 downto 0)))) & " to " & 
-                integer'image(to_integer(unsigned(baseram_data)));
-        end if;
-end process ; 
+    if(baseram_ce = '0' and baseram_oe = '0' and baseram_we = '1') then
+      baseram_data <= transport memory(to_integer(unsigned(baseram_addr(9 downto 0)))) after DELAY
+    elsif (baseram_ce = '0' and baseram_oe = '1' and baseram_we = '0') then
+        memory(to_integer(unsigned(baseram_addr(9 downto 0)))) <= baseram_data;
+        --report "write base " & integer'image(to_integer(unsigned(baseram_addr(9 downto 0)))) & " to " & integer'image(to_integer(unsigned(baseram_data)));
+    else
+      baseram_data <= (others => 'Z');
+    end if;
+end process ;
 
-process(extraram_we, extraram_data_in, extraram_addr, extraram_oe, extraram_ce)
+process(extraram_we, extraram_data, extraram_addr, extraram_oe, extraram_ce)
 begin
-    -- Write to extraMemory
-        if rising_edge(extraram_we) then 
-            memory(to_integer(unsigned(extraram_addr(9 downto 0)))) <= extraram_data_in;
-            report "write extra " & integer'image(to_integer(unsigned(extraram_addr(9 downto 0)))) & " to " & 
-                integer'image(to_integer(unsigned(extraram_data_in)));
-        end if;
-end process ; 
+  if(extraram_ce = '0' and extraram_oe = '0' and extraram_we = '1') then
+    extraram_data <= transport memory(to_integer(unsigned(extraram_addr(9 downto 0)))) after DELAY
+  elsif (extraram_ce = '0' and extraram_oe = '1' and extraram_we = '0') then
+      memory(to_integer(unsigned(extraram_addr(9 downto 0)))) <= extraram_data;
+      --report "write base " & integer'image(to_integer(unsigned(extraram_addr(9 downto 0)))) & " to " & integer'image(to_integer(unsigned(extraram_data)));
+  else
+    extraram_data <= (others => 'Z');
+  end if;
+end process ;
 
 END;
