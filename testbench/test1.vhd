da61da97d5b230d0f89bd6bc4054ac65958843bd
潘星宇 <pxy14@mails.tsinghua.edu.cn>
2016-12-08 02:04:14 +0800

    change ram_simulate

--- testbench/test1.vhd
+++ testbench/test1.vhd
@@ -95,12 +95,12 @@ ARCHITECTURE behavior OF test1 IS
    signal serialport_rxd : std_logic := '0';
 
   --BiDirs
-   signal baseram_data_in : std_logic_vector(31 downto 0);
-   signal extraram_data_in : std_logic_vector(31 downto 0);
-   signal baseram_data_out : std_logic_vector(31 downto 0);
-   signal extraram_data_out : std_logic_vector(31 downto 0);
-   signal baseram_data_backup : std_logic_vector(31 downto 0);
-   signal extraram_data_backup : std_logic_vector(31 downto 0);
+   signal baseram_data : std_logic_vector(31 downto 0);
+   signal extraram_data : std_logic_vector(31 downto 0);
+   --signal baseram_data_out : std_logic_vector(31 downto 0);
+   --signal extraram_data_out : std_logic_vector(31 downto 0);
+   --signal baseram_data_backup : std_logic_vector(31 downto 0);
+   --signal extraram_data_backup : std_logic_vector(31 downto 0);
    signal flash_data : std_logic_vector(15 downto 0);
 
   --Outputs
@@ -139,12 +139,12 @@ BEGIN
           rst => rst,
           clk => clk,
           baseram_addr => baseram_addr,
-          baseram_data => baseram_data_backup,
+          baseram_data => baseram_data,
           baseram_ce => baseram_ce,
           baseram_oe => baseram_oe,
           baseram_we => baseram_we,
           extraram_addr => extraram_addr,
-          extraram_data => extraram_data_backup,
+          extraram_data => extraram_data,
           extraram_ce => extraram_ce,
           extraram_oe => extraram_oe,
           extraram_we => extraram_we,
@@ -165,25 +165,23 @@ BEGIN
 
    ram_simulate0: ram_simulate PORT MAP (
           baseram_addr => baseram_addr, 
-          baseram_data_in => baseram_data_in, 
-          baseram_data_out => baseram_data_out,
+          baseram_data => baseram_data,
           baseram_ce => baseram_ce, 
           baseram_we => baseram_we,
           baseram_oe => baseram_oe, 
           extraram_addr => extraram_addr, 
-          extraram_data_out => extraram_data_out, 
-          extraram_data_in => extraram_data_in,
+          extraram_data => extraram_data,
           extraram_ce => extraram_ce, 
           extraram_we => extraram_we, 
           extraram_oe => extraram_oe
     );
 
-  process(baseram_data_backup, baseram_ce, baseram_we, baseram_oe)
-  begin
-    if(baseram_ce = '0' and baseram_oe = '1' and baseram_we = '0') then
-      baseram_data_in <= baseram_data_backup;
-    end if;
-  end process;
+  --process(baseram_data_backup, baseram_ce, baseram_we, baseram_oe)
+  --begin
+  --  if(baseram_ce = '0' and baseram_oe = '1' and baseram_we = '0') then
+  --    baseram_data_in <= baseram_data_backup;
+  --  end if;
+  --end process;
 
   --process(baseram_data_out, baseram_ce, baseram_we, baseram_oe)
   --begin
@@ -194,11 +192,11 @@ BEGIN
   --  end if;
   --end process;
 
-  baseram_data_backup <= baseram_data_out;
+  --baseram_data_backup <= baseram_data_out;
    --baseram_data_in <= baseram_data_backup when baseram_ce = '0' and baseram_we = '0' and baseram_oe = '1' else (others => 'Z');
-   extraram_data_in <= extraram_data_backup when extraram_ce = '0' and extraram_we = '0' and extraram_oe = '1' else (others => 'Z');
+   --extraram_data_in <= extraram_data_backup when extraram_ce = '0' and extraram_we = '0' and extraram_oe = '1' else (others => 'Z');
    --baseram_data_backup <= baseram_data_out when baseram_ce = '0' and baseram_we = '1' and baseram_oe = '0' else (others => 'Z');
-   extraram_data_backup <= extraram_data_out when extraram_ce = '0' and extraram_we = '1' and extraram_oe = '0' else (others => 'Z');
+   --extraram_data_backup <= extraram_data_out when extraram_ce = '0' and extraram_we = '1' and extraram_oe = '0' else (others => 'Z');
    
    -- Clock process definitions
    clk_process :process
