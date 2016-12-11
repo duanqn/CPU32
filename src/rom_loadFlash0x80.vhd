3aca6a615be3810a775db45ef09e3238a8f93819
Max Duan <duanqn@yeah.net>
2016-12-12 03:32:36 +0800

    Add test version of ROM`

---
+++ src/rom_loadFlash0x80.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use work.CPU32.all;
+
+
+package rom is
+
+constant ROM_SIZE : integer := 32;
+TYPE ROM is array(0 to ROM_SIZE - 1) of std_logic_vector(31 downto 0);
+
+constant boot_rom : ROM := (
+x"3C049E00",
+x"34020080",
+x"3C058000",
+x"00631826",
+x"00834021",
+x"95010000",
+x"95070002",
+x"00073C00",
+x"00270825",
+x"00A34021",
+x"AD010000",
+x"24630004",
+x"00433023",
+x"1CC0FFF6",
+x"00000000",
+x"34010020",
+x"3C03BFD0",
+x"346303F8",
+x"AC610000",
+x"AC610000",
+x"AC610000",
+x"00A00008",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000",
+x"00000000"    );
+
+end rom;
