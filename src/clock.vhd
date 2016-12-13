6c55d8ada029f01cca9a45f0cdd05b66d5ccc4ec
潘星宇 <pxy14@mails.tsinghua.edu.cn>
2016-12-13 19:17:06 +0800

    fix bugs  the mem

--- src/clock.vhd
+++ src/clock.vhd
@@ -19,18 +19,14 @@ BEGIN
 
   PROCESS(clk)
     BEGIN
-      if(rst = '0') then 
-        clk_2 <= '0';
-      elsif (clk'event and clk = '1') then
+      if (clk'event and clk = '1') then
         clk_2 <= not clk_2;
       END IF;
     END PROCESS;
 
   PROCESS(clk_2)
     BEGIN
-      if(rst = '0') then 
-        clk_4 <= '0';
-      elsif (clk_2'event and clk_2 = '1') then
+      if (clk_2'event and clk_2 = '1') then
         clk_4 <= not clk_4;
       END IF;
     END PROCESS;
